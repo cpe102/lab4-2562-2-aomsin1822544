6511ffe3513a1d7562a5956b89391ba0621756fb
aomsin1822544 <pspsas333_111@hotmail.com>
2020-01-03 17:06:57 +0700

    v.1

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int findDivisor(int n){
+    int a=2;
+    if(n>1){
+        while(n>a){
+        if(n%a==0){
+            return a;
+        }
+        else
+        {
+            a++;
+        }
+        
+        }
+    }
+     
+
+}
+   
+ 
+ int main(){
+   int x,y;
+   cin >> x;
+   cout << findDivisor(x);}
+
+    
+       
+       
+        
+       
+
 
