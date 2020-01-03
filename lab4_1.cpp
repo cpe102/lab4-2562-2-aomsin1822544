cf08c670d529992c26efdeeba1353a507fd21dd6
aomsin1822544 <pspsas333_111@hotmail.com>
2020-01-03 15:56:23 +0700

    4.1

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
 
+double calculate(double x){ 
+     double y=3*pow(x,3)+2*2.71828+pow(2,2*x+1)+sqrt(pow(x,2)+1);
+       return  y;
+      
+}
+ int main(){
+     double n;
+     cin >> n;
+     cout << "Result=" << calculate(n);
+
+
+     return 0; 
+     
+  
+ }
+      
+     
